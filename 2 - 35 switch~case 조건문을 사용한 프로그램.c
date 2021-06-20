4513c30b8909d8a762febf35eeece4cb3cbbcb41
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-06-20 21:56:24 +0900

    [switch] 나라를 입력하여 그의 수도를 알려줌

---
+++ 2 - 35 switch~case 조건문을 사용한 프로그램.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int cap;
+	printf("다음 국가의 수도는? (1.미국 2.일본 3.중국)\n");
+	scanf_s("%d", &cap);
+	switch (cap) {
+	case 1: printf("미국 : 워싱턴 D.C. \n"); break;
+	case 2:printf("일본 : 도쿄 \n"); break;
+	case 3:printf("중국 : 베이징 \n"); break;
+	default: printf("입력 오류입니다. \n"); break;
+	}
+	return 0;
+}
