3b9a9d2858829ccbd6a87a12854f941137d674fe
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-10-25 22:05:34 +0900

    자판기

--- 주문.c
+++ 주문.c
@@ -1,50 +1,41 @@
 ﻿#include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
 
 //반환형과 매개변수가 있다면 각각의 의미를 서술하시오
-int order(int money, int num, int sunnyten, int sweat, int change);
+int order(int);
+int order(int deposit);
+int deposit = 3000;
+int coca = 700, sunny = 600, blue = 800, num;
 
-void main(void)
-{
-	//금액
-	int money = 3000;
-	//음료
-	int coke = 700, sunnyten = 600, sweat = 800;
-	int num;
-	int change;
+void main(void) {
 
-	while (1)
-	{
-		change = order(&money, &num, &coke, &sunnyten, &sweat, &change);
-		printf("%d", change);
-		printf("\n");
+    while (deposit > 0)
+    {
+        
 
-		if (change <= 0)
-			break;
-	}
+        printf("현재금액 %d\n", deposit);
+        printf("1. 코카콜라:700원\n");
+        printf("2. 써니텐:600원\n");
+        printf("3. 포카리스웨트:800원\n");
+        printf("구매할 물건의 버튼을 입력하시오 ");
+        scanf_s("%d", &num);
+
+        deposit = order(deposit);
+    }
 }
 
-int order(int money, int num, int coke, int sunnyten, int sweat, int change)
+int order(int deposit)
 {
-	int temp;
-	printf("현재금액 %d\n", money);
-	printf("1. 코카콜라:700원\n");
-	printf("2. 써니텐:600원\n");
-	printf("3. 포카리스웨트:800원\n");
-	printf("구매할 물건의 버튼을 입력하시오 ");
-	scanf_s("%d", &num);
-	
-	temp = num;
-	if (temp == 1)
-		change = money - coke;
-	else if (temp == 2)
-		change = money - sunnyten;
-	else if (temp == 3)
-		change = money - sweat;
-	else {
-		printf("다시 입력하세요 : ");
-		scanf_s("%d\n", &num);
-	}
-	
-	return change;
+    switch (num) {
+    case 1:
+        deposit -= coca;
+        break;
+    case 2:
+        deposit -= sunny;
+        break;
+    case 3:
+        deposit -= blue;
+        break;
+    }
+    printf("\n");
+    return deposit;
 }
