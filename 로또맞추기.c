1314dd5ca1dc0859a9dc4e60af86c318e795ee89
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-11-14 20:23:32 +0900

    로또 번호가 맞으면 1등에 당첨되었다는 문장 출력

---
+++ 로또맞추기.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+void confirm_1st(int answer, int my);
+
+void main(void)
+{
+	// 로또번호(오름차순 정렬)
+	int answer_number[6] = { 2, 3, 8, 11, 15, 41 };
+	// 자신이 기입한 번호(오름차순 정렬)
+	int my_number[6] = { 2, 3, 8, 11, 15, 41 };
+
+	if (confirm_1st) {
+		printf("1등에 당첨되었습니다. \n");
+	}
+}
+
+void confirm_1st(int answer, int my)
+{
+	if (answer == my)
+		return;
+}
