bdc40299deb9b9a1167f48c38c73fb9c6ee5ceeb
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-12-06 07:44:42 +0900

    [struct] 코카콜라, 써니텐, 포카리스웨트를 구조체로 구현

--- 구조체 과제.c
+++ 구조체 과제.c
@@ -11,7 +11,7 @@
 
 #include <stdio.h>
 
-void order(int* money, int button, int coca, int sunny, int pocari);
+void order(int* money, int button, struct drink cost);
 
 struct drink {
 	int coca;
@@ -35,27 +35,26 @@ void main(void)
 		scanf_s("%d", &num);
 		// 자판기에서 물건을 주문을 하는 함수 order를 호출한다.
 		// 코카콜라, 써니텐, 포카리스웨트 중 하나의 버튼을 누르면 order함수가 이를 처리한다.
-		order(&money, num, d1.coca, d1.sunny, d1.pocari);
+		order(&money, num, d1);
 
 		printf("\n");
 	}
 }
 
-// void order(int* money, int button, struct drink cost) // 힌트
-void order(int* money, int button, int coca, int sunny, int pocari)
+void order(int* money, int button, struct drink cost) // 힌트
 {
 	switch (button)
 	{
 	case 1:	//코카콜라 버튼을 눌렀을 경우
-		*money -= coca;
+		*money -= cost.coca;
 		break;
 
 	case 2: //써니텐 버튼을 눌렀을 경우
-		*money -= sunny;
+		*money -= cost.sunny;
 		break;
 
 	case 3: //포카리스웨트 버튼을 눌렀을 경우
-		*money -= pocari;
+		*money -= cost.pocari;
 		break;
 	}
 }
