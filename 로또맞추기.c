8ee7d3031ee2aba4b4ba7c9e60ef45ae0b1ff1e3
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2021-12-07 11:38:07 +0900

    로또맞추기 수정

--- 로또맞추기.c
+++ 로또맞추기.c
@@ -1,5 +1,5 @@
 ﻿#include <stdio.h>
-int confirm_1st(int answer, int my);
+int confirm_1st(int* answer, int* my);
 
 void main(void)
 {
@@ -8,13 +8,27 @@ void main(void)
 	// 자신이 기입한 번호(오름차순 정렬)
 	int my_number[6] = { 2, 3, 8, 11, 15, 41 };
 
-	if (confirm_1st) {
+	// HACK : 함수에 argument가 없었음
+	//if (confirm_1st) {
+	//	printf("1등에 당첨되었습니다. \n");
+	//}
+
+	if (confirm_1st(answer_number, my_number)) {
 		printf("1등에 당첨되었습니다. \n");
 	}
 }
 
-int confirm_1st(int answer, int my)
+// HACK : 매개변수를 주소값으로 해야함
+// int confirm_1st(int answer, int my)
+int confirm_1st(int* answer, int* my)
 {
-	if (answer == my)
-		return;
+	// HACK : answer와 my는 다른 공간에 할당되어 있음
+	// if (answer == my)
+	if (*answer == *my)
+		// HACK : 반환값이 존재하지 않음
+		// return;
+		return 1;
+	else
+		return 0;
 }
+
